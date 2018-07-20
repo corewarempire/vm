e79d7a7e782871237d9d2b645984a0c702ab104f
Axel DHONDT <adhondt@e3r10p9.42.fr>
2018-07-20 15:55:00 +0200

    auto commit

--- src/st.c
+++ src/st.c
@@ -7,7 +7,7 @@ void	st(t_board *bd, t_process *proc)
 	int value;
 
 	octal = ocp_scd(bd->ram[proc->pc + 1]);
-	first_reg = get_reg(proc, proc->pc + 2);
+	first_reg = get_reg(proc, bd->ram[proc->pc + 2]);
 	value = (octal & 3) ? get_indir(bd, proc->pc + 3) : get_reg(proc, proc->pc + 3);
 	proc->carry = (!first_reg) ? 1 : 0;
 	printf("Here : %d\n", first_reg);
