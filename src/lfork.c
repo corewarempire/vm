fef84f9d6aea56da62e1175f659833617bd3ebe7
Alexandre KARASSOULOFF <akarasso@e3r3p10.42.fr>
2018-07-21 11:32:08 +0200

    write all func, without any test... we need to check it each one..

--- src/lfork.c
+++ src/lfork.c
@@ -2,5 +2,11 @@
 
 void	lfork(t_board *bd, t_process *proc)
 {
-	
+	t_process	*new;
+	int			newpc;
+
+	proc->pc++;
+	newpc = get_params(bd, proc, &proc->pc, {T_DIR, 1});
+	if (!(new = add_process(board->lst_process, proc->player_id, newpc)))
+		printf("FAILED TO CREATE FORK NEED TO EXIT\n");
 }
--- src/lld.c
+++ src/lld.c
@@ -2,5 +2,16 @@
 
 void	lld(t_board *bd, t_process *proc)
 {
-	
+	unsigned char	ocp;
+	int				val1;
+	int				val2;
+
+	proc->pc++;
+	ocp = ocp_first(bd->ram[MEN_NOD(proc->pc++)]);
+	if (!(val1 = get_params(bd, proc, &proc->pc, {ocp, 1})))
+		proc->carry = 1;
+	else
+		proc->carry = 0;
+	proc->r[bd->ram[MEM_MOD(proc->pc)] - 1] = val1;
+	proc->pc++;
 }
