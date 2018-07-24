5e54e9c73aea406d4b21587f5af4964c3cd4825e
Alexandre KARASSOULOFF <akarasso@e3r11p8.42.fr>
2018-07-24 15:33:32 +0200

    test folder

---
+++ tests/live.s
@@ -0,0 +1,19 @@
+.name "Ca#ndy"
+.comment "d#433#"
+
+label1:
+label1:
+label1:
+live %-2147483648
+live %2147483648
+live %214748364732488
+live %:label1
+label1:
+live %:label1
+live %2147483647
+label1:
+live %10
+live %-10
+live %:label2
+label2:
+autrelabel:
