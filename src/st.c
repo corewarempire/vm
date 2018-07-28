2c6bd284bc066a411b6e6f311dcddc533b68770e
Alexandre KARASSOULOFF <akarasso@e3r4p5.42.fr>
2018-07-28 20:40:09 +0200

    st

--- src/st.c
+++ src/st.c
@@ -12,27 +12,20 @@ void	st(t_board *bd, t_process *proc)
 {
 	unsigned int	pc;
 	unsigned int	ocp;
-	unsigned int	first_reg;
+	unsigned int	value_reg;
 	unsigned int	offset;
 	int				value;
 
-	pc = proc->pc + 1;
+	pc = proc->pc;
 	printf("st:\n");
-	ocp = ocp_scd(bd->ram[MEM_MOD(pc)]);
-	pc++;
-	first_reg = get_params(bd, proc, &pc, (int[3]){T_REG, 0, 0});
-		if (bd->verbose[1])
-	{
-	ft_putstrnbrstr("Player ", proc->id_player, " // Process ");
-	ft_putnbrstrnbr(proc->id_process, "\nSt r", bd->ram[MEM_MOD(pc - 1)]);
-	}
-	value = get_params(bd, proc, &pc, (int[3]){ocp, 0, 0});
-	proc->carry = (!first_reg) ? 1 : 0;
-	if (ocp == IND_CODE)
-		set_ramvalue(bd, proc->pc + (value % IDX_MOD), first_reg);
-	else if (ocp == REG_CODE)
-		proc->r[value - 1] = first_reg;
+	ocp = ocp_scd(bd->ram[MEM_MOD(pc + 1)]);
+	value_reg = proc->r[bd->ram[MEM_MOD(pc + 2)] - 1];
+	value = (ocp == REG_CODE) ? bd->ram[MEM_MOD(pc + 3)] : get_indir(bd, proc, pc + 3);
+	if (ocp == REG_CODE)
+		proc->r[value - 1] = value_reg;
+	else
+		set_ramvalue(bd, proc->pc + (value % IDX_MOD), value_reg);
 	if (bd->verbose[1])
-		verbosity(ocp, proc, value, first_reg);
+		verbosity(ocp, proc, value, value_reg);
 	proc->pc += (ocp == IND_CODE) ? 5 : 4;
 }
