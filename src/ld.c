a0f2c08b3b72fb82ba0cc1912b5a0dad1ed2718a
Alexandre KARASSOULOFF <akarasso@e3r4p5.42.fr>
2018-07-28 19:55:39 +0200

    remove debug msg

--- src/ld.c
+++ src/ld.c
@@ -3,12 +3,11 @@
 void	ld(t_board *bd, t_process *proc)
 {
 	unsigned int	pc;
-	unsigned int	octal;
+	unsigned int	ocp;
 	unsigned int	offset;
 	int				reg;
 	int				val1;
 
-<<<<<<< HEAD
 	pc = proc->pc + 1;
 	// printf("ld:\n");
 	ocp = ocp_first(bd->ram[MEM_MOD(pc)]);
@@ -19,15 +18,6 @@ void	ld(t_board *bd, t_process *proc)
 		proc->carry = 0;
 	proc->r[bd->ram[MEM_MOD(pc)] - 1] = val1;
 	proc->pc = pc + 1;
-=======
-	pc = proc->pc;
-	octal = ocp_first(bd->ram[MEM_MOD(pc + 1)]);
-	val1 = (octal == DIR_CODE) ? get_dir4(bd, pc + 2) : get_indir(bd, proc, pc + 2);
-	offset = (octal == DIR_CODE) ? 6 : 4;
-	reg = bd->ram[MEM_MOD(pc + offset)];
-	proc->carry = (!(proc->r[reg - 1] = val1)) ? 1 : 0;
-	proc->pc += offset + 1;
->>>>>>> 2a6a7fb0c7e8a732f6a4238e39fd333f3d2463da
 	if (!bd->verbose[1])
 		return ;
 	ft_putstrnbrstr("Player ", proc->id_player, " // Process ");
