ef4caaef537428257ba29af5b0e6cc5185ae0d11
Axel DHONDT <adhondt@e3r2p4.42.fr>
2018-07-29 18:23:34 +0200

    Add and verbose

--- axeltest/and_test.s
+++ axeltest/and_test.s
@@ -2,13 +2,27 @@
 .comment "Ha, Ha, Ha, stayiiiiin' aliiiiiiiiiive"
 	
 #sti	r1, %:live, %1			;change live by the right value
-ld %5, r4
-ld %6, r5
-and r4, r5, r6
-and %10, r5, r6
-and 10, r5, r6
-and r4, 0, r6
+#ld %200, r6
+ld %1, r5
+add	r5, r1, r16
+ld %15, r3
+amd
+ld %17000, r3
+ld %-1, r6
+ld %-2000000000, r6
+ld %-4000000000, r6
+ld %1500000000, r6
+ld %4000000000, r6
 
+
+ld 1, r5
+ld 15, r3
+ld 17000, r3
+ld -1, r6
+ld -2000000000, r6
+ld -4000000000, r6
+ld 1500000000, r6
+ld 4000000000, r6
 #live:
 #live %1
 #zjmp %:live
