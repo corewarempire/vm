2fa606e603afb5184e922a7e19ed72fd7a3c610c
Alexandre KARASSOULOFF <akarasso@e3r4p5.42.fr>
2018-07-29 02:31:59 +0200

    lld

--- src/lld.c
+++ src/lld.c
@@ -1,26 +1,30 @@
 #include "corewar.h"
 
-void	lld(t_board *bd, t_process *proc)
-{
-	unsigned int	pc;
-	int				val1;
-	unsigned char	ocp;
 
-	pc = proc->pc + 1;
-	printf("lld\n");
-	ocp = ocp_first(bd->ram[MEM_MOD(pc)]);
-	pc++;
-	if (!(val1 = get_params(bd, proc, &pc, (int[3]){ocp, 0, 1})))
-		proc->carry = 1;
-	else
-		proc->carry = 0;
-	proc->r[bd->ram[MEM_MOD(pc)] - 1] = val1;
-	proc->pc = MEM_MOD(pc + 1);
-	if (!bd->verbose[1])
-		return ;
+//{"lld", 2, {T_DIR | T_IND, T_REG}, 13, 10, "long load", 1, 0},
+
+static void	verbosity(t_board *bd, t_process *proc, int ocp, int val)
+{
 	ft_putstrnbrstr("Player ", proc->id_player, " // Process ");
-	ft_putnbrstrnbr(proc->id_process, "\nLld ", val1);
-	ft_putstrnbrstr(" in r", bd->ram[MEM_MOD(pc)], ". Carry : ");
+	ft_putnbrstrnbr(proc->id_process, "\nLld ", val);
+	ft_putstrnbrstr(" in r", bd->ram[MEM_MOD(pc + 4)], ". Carry : ");
 	ft_putnbr(proc->carry);
 	ft_putstr("\n\n");
+}
+
+void	lld(t_board *bd, t_process *proc)
+{
+	int	ocp;
+	int	value;
+
+	ocp = ocp_first(bd->ram[MEM_MOD(proc->pc + 1)]);
+	value = (ocp == DIR_CODE) ? get_dir4(bd, proc-pc + 2): get_long_indir(bd, proc, proc-pc + 2);
+	proc->carry = (!value) ? 1 : 0;
+	if (ocp == DIR_CODE)
+		proc->r[bd->ram[MEM_MOD(proc->pc + 6)] - 1] = value;
+	else
+		proc->r[bd->ram[MEM_MOD(proc->pc + 4)] - 1] = get_dir4(bd, proc->pc + value);
+	if (!bd->verbose[1])
+		verbosity(bd, proc, ocp, val7ue);
+	proc->pc += (ocp == DIR_CODE) ?  : 5;
 }
