0829dba2b12f9f5d501f93cc6938ad5cad21e605
Alexandre KARASSOULOFF <akarasso@e3r3p3.42.fr>
2018-07-29 17:43:55 +0200

    ld

--- src/ld.c
+++ src/ld.c
@@ -1,28 +1,29 @@
 #include "corewar.h"
 
-void	ld(t_board *bd, t_process *proc)
-{
-	unsigned int	pc;
-	unsigned int	ocp;
-	unsigned int	offset;
-	int				reg;
-	int				val1;
+// {"ld", 2, {T_DIR | T_IND, T_REG}, 2, 5, "load", 1, 0},
 
-	pc = proc->pc + 1;
-	// printf("ld:\n");
-	ocp = ocp_first(bd->ram[MEM_MOD(pc)]);
-	pc++;
-	if (!(val1 = get_params(bd, proc, &pc, (int[3]){ocp, 0, 0})))
-		proc->carry = 1;
-	else
-		proc->carry = 0;
-	proc->r[bd->ram[MEM_MOD(pc)] - 1] = val1;
-	proc->pc = pc + 1;
-	if (!bd->verbose[1])
-		return ;
+static void	verbosity(t_board *bd, t_process *proc, int value, int reg)
+{
 	ft_putstrnbrstr("Player ", proc->id_player, " // Process ");
-	ft_putnbrstrnbr(proc->id_process, "\nLd ", val1);
-	ft_putstrnbrstr(" in r", bd->ram[MEM_MOD(pc)], ". Carry : ");
+	ft_putnbrstrnbr(proc->id_process, "\nLd ", value);
+	ft_putstrnbrstr(" in r", reg, ". Carry : ");
 	ft_putnbr(proc->carry);
 	ft_putstr("\n\n");
+}
+
+void	ld(t_board *bd, t_process *proc)
+{
+	int	ocp;
+	int	offset;
+	int	reg;
+	int	value;
+
+	ocp = ocp_first(bd->ram[MEM_MOD(proc->pc + 1)]);
+	value = (ocp == IND_CODE) ? get_indir(bd, proc->pc + 2): get_dir4(bd, proc->pc + 2);
+	proc->carry = (!value) ? 1 : 0;
+	reg = (ocp == IND_CODE) ? bd->ram[MEM_MOD(proc->pc + 4)] : bd->ram[MEM_MOD(proc->pc + 6)];
+	proc->r[reg - 1] = value;
+	proc->pc += (ocp == IND_CODE) ? 5 : 7;
+	if (bd->verbose[1])
+		verbosity(bd, proc, value, reg);
 }
